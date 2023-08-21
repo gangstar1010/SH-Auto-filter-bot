0e3bcb7d628ce410ddb7904c2fe283b684124b0b
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2023-08-21 22:47:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/gangstar1010/SH-Auto-filter-bot /Auto-search-tamil-bot
+  git clone https://github.com/gangstar1010/SH-Auto-filter-bot /SH-Auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /SH-Auto-filter-bot
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
