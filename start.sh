db7b93c3267826916251ab3e566ec7d01b60b100
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2023-08-22 10:49:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/gangstar1010/SH-Auto-filter-bot /SH-Auto-filter-bot
+  git clone https://github.com/gangstar1010/SH-Auto-filter-bot.git /SH-Auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SH-Auto-filter-bot
