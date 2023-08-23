484823fd8cda12c3e36330777e6c3c129f474d84
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2023-08-23 14:58:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdisk.pro')
-SHORTLINK_API = environ.get('SHORTLINK_API', '159924d9bdc95bfdb6f5d5df6e2ca75caff6d673)
+SHORTLINK_API = environ.get('SHORTLINK_API', '159924d9bdc95bfdb6f5d5df6e2ca75caff6d673')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
